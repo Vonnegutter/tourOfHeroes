db2d4c6e404b6f57ad2612b71e28d4a026dd0e2e
Molly Ellison <molly.ellison@us.softvision.global>
2021-10-06 15:02:09 -0500

    Initial Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Joan Jett' },
+  { id: 12, name: 'The Raincoats' },
+  { id: 13, name: 'The Slits' },
+  { id: 14, name: 'X-Ray Spex' },
+  { id: 15, name: 'Peaches' }
+];
